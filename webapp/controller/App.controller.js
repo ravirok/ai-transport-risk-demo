0d3f958704f6498d64d29d6423e2f7ad56cc5f03
ravirok <ravirok@users.noreply.github.com>
2026-03-13 10:37:19 +0530

    Add files via upload

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,1 +1,42 @@
+sap.ui.define([
+ "sap/ui/core/mvc/Controller",
+ "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
 
+ return Controller.extend("demo.controller.App", {
+
+  onInit: function () {
+
+   fetch("/api/transports")
+   .then(res => res.json())
+   .then(data => {
+
+    data.transports.forEach(t => {
+
+     if (t.risk_score < 0.3){
+       t.status="SAFE"
+       t.state="Success"
+     }
+
+     else if (t.risk_score < 0.6){
+       t.status="MEDIUM"
+       t.state="Warning"
+     }
+
+     else{
+       t.status="HIGH"
+       t.state="Error"
+     }
+
+    });
+
+    var model = new JSONModel(data);
+    this.getView().setModel(model);
+
+   });
+
+  }
+
+ });
+
+});
