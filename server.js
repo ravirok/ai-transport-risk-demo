894b50684cb09b4450788e2bddc54e6492d79317
ravirok <ravirok@users.noreply.github.com>
2026-03-18 13:16:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ async function callAI(transports) {
 
     const AI_URL =
       `${ai.serviceurls.AI_API_URL}` +
-      `/v2/inference/deployments/de0489d59e5a01b8/invocations`;
+      `/v2/inference/deployments/de0489d59e5a01b8/v2/invocations`;
 
     const payload = {
       input: transports.map(tr => ({
